d0fe4c45d666f1c645cbb6c67906aa242aca6778
carol1newang <cw4265@nyu.edu>
2025-10-29 14:19:44 -0400

    3rd push

--- p5_Starter_Code/sketch.js
+++ p5_Starter_Code/sketch.js
@@ -15,6 +15,8 @@ function setup(){
 }
 
 function draw(){
+  start();
+  /*
   runTime = millis() - runEnd;
 
   if (runTime < 2000){
@@ -34,7 +36,79 @@ function draw(){
   else {
     runEnd = millis ();
     reset();
-  }
+  }*/
+}
+
+function start(){
+  noStroke();
+  fill(255);
+  beginShape();
+  vertex(50, 220);
+  vertex(440, 200);
+  vertex(460, 450);
+  vertex(70, 470);
+  endShape(CLOSE);
+
+  fill(100, 50, 255);
+  beginShape();
+  vertex(120, 220);
+  bezierVertex(70, 290, 160, 330, 110, 380);
+  bezierVertex(80, 430, 170, 480, 130, 520);
+  bezierVertex(100, 570, 150, 560, 150, 580);
+
+  vertex(380, 580);
+  vertex(360, 400);
+  endShape(CLOSE);
+
+  fill(150, 200, 255);
+  beginShape();
+  vertex(130, 250);
+  vertex(130, 340);
+  bezierVertex(130, 580, 370, 580, 370, 340);
+  vertex(370, 250);
+  endShape();
+
+  beginShape();
+  vertex(220, 400);
+  vertex(310, 400);
+  vertex(320, 550);
+  vertex(230, 550);
+  endShape();
+
+  fill(100, 50, 255);
+  beginShape();
+  vertex(110, 280);
+  bezierVertex(90, 120, 380, 120, 380, 280);
+  bezierVertex(370, 290, 190, 260, 180, 250);
+  bezierVertex(180, 260, 115, 290, 110, 280);
+  endShape(); 
+
+  beginShape();
+  vertex(330, 220);
+  bezierVertex(280, 290, 370, 330, 320, 380);
+  bezierVertex(290, 430, 380, 480, 340, 520);
+  bezierVertex(310, 570, 360, 560, 360, 580);
+
+  bezierVertex(370, 560, 350, 580, 390, 480);
+  bezierVertex(400, 410, 350, 400, 380, 350);
+  bezierVertex(400, 300, 380, 290, 380, 280);
+  endShape(CLOSE);
+
+  fill(255);
+  beginShape();
+  vertex(0, 550);
+  vertex(600, 530);
+  vertex(600, 600);
+  vertex(0, 600);
+  endShape(CLOSE);
+
+  stroke(100, 50, 255);
+  strokeWeight(5);
+  line(155, 320, 180, 310);
+  line(290, 310, 260, 305);
+
+  ellipse(165, 340, 5, 5);
+  ellipse(270, 335, 5, 5);
 }
 
 
